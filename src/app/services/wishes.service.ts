0e794bddcd2560eeae822c53fb0f29703a6e4076
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-02-10 22:27:27 -0500

    Se inyecto modelo list.model.ts al servicio y se inyecto servicio wishes.services.ts a tab1.component.ts y a tab2.component.ts

--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { List } from '../models/list.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WishesService {
 
-  lists: any[] = [];
+  lists: List[] = [];
 
-  constructor() { }
+  constructor() {
+    console.log("Servicio inicializado")
+  }
 }
