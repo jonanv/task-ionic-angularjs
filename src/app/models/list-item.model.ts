637c13b00648f386cc7badc2384a77e8c64c3724
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-02-10 22:14:41 -0500

    Se creo carpeta modelos y se inicializo el model list-item.model.ts

---
+++ src/app/models/list-item.model.ts
@@ -0,0 +1,9 @@
+export class ListItem {
+
+    descripcion: string;
+    completado: string;
+
+    constructor(descripcion: string) {
+        this.descripcion = descripcion;
+    }
+}
--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class WishesService {
 
+  lists: any[] = [];
+
   constructor() { }
 }
