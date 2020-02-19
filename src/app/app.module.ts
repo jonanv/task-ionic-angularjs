13e6470942c278608460601b1ddaa830f07db4cb
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-02-19 00:06:03 -0500

    Se soluciono error en el modulo add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddPipe } from './pages/add.pipe';
 
 @NgModule({
-  declarations: [AppComponent, AddPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
