3a71f3fee56012350c6da179efa67b25985a103d
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-02-22 15:59:54 -0500

    Se agrego mensaje de alerta al metodo add en tab1.page.ts y se modifico metodo a async

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WishesService } from '../../services/wishes.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,14 +12,44 @@ export class Tab1Page {
 
   constructor(
     public wishesService: WishesService,
-    private router: Router
+    private router: Router,
+    private alertController: AlertController
   ) {
-    
+
   }
 
-  addList() {
+  async addList() {
     // this.router.navigate(['/tabs/tab1/add']);
-    this.router.navigateByUrl('/tabs/tab1/add');
+    // this.router.navigateByUrl('/tabs/tab1/add');
+
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar');
+        }
+      },
+      {
+        text: 'Crear',
+        handler: (data) => {
+          if (data.titulo.length === 0) {
+            return;
+          }
+          else {
+            // TODO: Crear la lista
+          }
+        }
+      }]
+    });
+
+    await alert.present();
   }
 
 }
