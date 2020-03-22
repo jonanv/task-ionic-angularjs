8aba4cbb287793716a156583b193742c3c31ff46
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-03-22 17:52:30 -0500

    Se importo components.module.ts a tab1.module.ts y page2.module.ts para importar el lists.component.ts y evitar error por no llamarse desde un componente susperior

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -4,15 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ListsComponent } from '../../components/lists/lists.component';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ComponentsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page, ListsComponent]
+  declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
